54766d51785c65005e0e5edb37c76629a1e02a5d
EC2 Default User <ec2-user@ip-172-31-41-148.ec2.internal>
2025-02-13 18:32:01 +0000

    modified books.py and main.py

--- api/routes/books.py
+++ api/routes/books.py
@@ -32,6 +32,7 @@ db.books = {
     ),
 }
 
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_book(book: Book):
     db.add_book(book)
@@ -39,11 +40,13 @@ async def create_book(book: Book):
         status_code=status.HTTP_201_CREATED, content=book.model_dump()
     )
 
+
 @router.get(
     "/", response_model=OrderedDict[int, Book], status_code=status.HTTP_200_OK
 )
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
+
 
 @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def get_book_by_id(book_id: int) -> Book:
@@ -52,12 +55,14 @@ async def get_book_by_id(book_id: int) -> Book:
         raise HTTPException(status_code=404, detail="Book not found")
     return book
 
+
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
     return JSONResponse(
         status_code=status.HTTP_200_OK,
         content=db.update_book(book_id, book).model_dump(),
     )
+
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
